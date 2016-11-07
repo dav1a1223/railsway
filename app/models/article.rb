1f60f98c66fabdae5b3fb585825754990add3c40
dalai <dav1a1223@gmail.com>
2016-11-08 00:46:31 +0800

    move crawler from lib to model concern

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,3 +1,4 @@
 class Article < ApplicationRecord
+  include Crawlable
 
 end
--- app/models/concerns/crawlable.rb
+++ app/models/concerns/crawlable.rb
@@ -1,11 +1,20 @@
-require 'nokogiri'
-require 'open-uri'
-
 module Crawlable
   extend ActiveSupport::Concern
 
-  def self.crawling
-    rubyflow = Nokogiri::HTML(open('http://www.rubyflow.com/'))
-    rubyflow.css('article div h1 a').map(&:content)
+  class_methods do
+    def rubyflow
+      rubyflow = Nokogiri::HTML(open('http://www.rubyflow.com/'))
+      rubyflow.css('article div[@class="body"]').each do |info|
+        if info.content.present?
+          article = Article.new
+          article.title = info.children[1].children[0].children.text
+          article.description = info.children[3].children.text.strip
+          article.from = "Rubyflow"
+          article.url = "http://www.rubyflow.com" + info.children[1].children[0].attributes["href"]
+          article.author = info.children[5].children[0].children[2].text.strip
+          article.save!
+        end
+      end
+    end
   end
 end

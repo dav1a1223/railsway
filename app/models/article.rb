61d9b65caff7fb16af67b90a89bd018fdda59734
dalai <dav1a1223@gmail.com>
2016-11-12 17:08:25 +0800

    add category table

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,4 +1,5 @@
 class Article < ApplicationRecord
+  belongs_to :category
   include Crawlable
 
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161107134309) do
+ActiveRecord::Schema.define(version: 20161112084458) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -23,7 +23,16 @@ ActiveRecord::Schema.define(version: 20161107134309) do
     t.datetime "updated_at",  null: false
     t.string   "author"
     t.text     "description"
+    t.integer  "category_id"
+    t.index ["category_id"], name: "index_articles_on_category_id", using: :btree
     t.index ["title"], name: "index_articles_on_title", using: :btree
+  end
+
+  create_table "categories", force: :cascade do |t|
+    t.string   "name"
+    t.text     "description"
+    t.datetime "created_at",  null: false
+    t.datetime "updated_at",  null: false
   end
 
   create_table "users", force: :cascade do |t|
